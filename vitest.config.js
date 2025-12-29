2ecc8e9e4cbda1db97be72e67655229e6428b735
Iván Campaña <ivan.campana@gmail.com>
2025-12-29 21:30:51 +0000

    fix: added missing test dependencies

--- vitest.config.js
+++ vitest.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vitest/config';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
 
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      $lib: path.resolve('./src/lib')
+    }
+  },
   test: {
     environment: 'jsdom',
     setupFiles: ['./src/tests/setup.js'],
